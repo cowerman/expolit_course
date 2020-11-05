96420535d8529d06032fea1a23dda7061f4d6cdb
qsort987 <prj_share@sina.com>
2020-11-05 11:18:18 +0800

    modify the first vulnerable secure code.
    overflow the stack and use the where function's address to replace the
    return address in the stack.
    
    Signed-off-by: qsort987 <prj_share@sina.com>

--- test2_overflow/overflow1.c
+++ test2_overflow/overflow1.c
@@ -1,30 +1,41 @@
 /*************************************************************************
-    > File Name: overflow1.c
+    > File Name: first_vol.c
     > Author:		
     > Mail:		
-    > Created Time: Wed 02 Sep 2020 07:04:12 PM CST
+    > Created Time: 2020年11月03日 星期二 14时36分54秒
  ************************************************************************/
+
+/* For 64Bit Operation machine */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void why_here(void)
+/* before stack frame					 |		after override stack frame
+---0x7fffdd90--|   X                  | <----RBP0	 |	---0x7fffdd90--|   X                  | <----RBP0
+---0x7fffdd88--|   return IP          | <----RSP0	 |	---0x7fffdd88--|   return where IP    | <----RSP0
+---0x7fffdd80--|   RBP0(0x7fffdd90)   | <----RSP1,RBP1	 |	---0x7fffdd80--|   0x00030002         | <----RSP1,RBP1
+---0x7fffdd7c--|   4Byte Local var    | <----RSP2	 |	---0x7fffdd7c--|   1                  | <----RSP2
+ */
+void where(void)
 {
+	printf("why are u here!\n");
 
-	printf("Why are you here!...\n");
-	exit(0);
+	return;
+	//exit(0);
 }
 
 void f()
 {
-	int buf; int *p = &buf;
-	*(p + 2) = (int)why_here;
-
+	int repl[1];
+	repl[0] = 1;
+	repl[1] = 2;
+	repl[2] = 3;
+	repl[3] = (int)where;
 }
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	f();
 
+	f();
 	return 0;
 }
